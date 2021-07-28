ace2dc8963eb1e9e53b397b65676387802adbf37
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2021-07-28 13:43:30 +0400

    перенес каркас

--- EMT/Classes/Misc/GlobalDefinitions.swift
+++ EMT/Classes/Misc/GlobalDefinitions.swift
@@ -7,17 +7,17 @@
 
 final class GlobalDefinitions {
     static let domainUrl = "https://exams.korrekted.com"
-    static let apiKey = "M1cnYpenrNNxXL6M"
+    static let apiKey = "fy2wuEZSiOP6xeoZ"
 
     static let sdkDomainUrl = "https://analytics.korrekted.com"
-    static let sdkApiKey = "ZhsVz1QCYGcrzfww"
+    static let sdkApiKey = "pTD5SAPWu8y7C9z7"
 
-    static let applicationTag = "EXAM_EMT"
+    static let applicationTag = "EXAM_MCAT"
 
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
 
-    static let appleAppID = "1568136144"
+    static let appleAppID = "1568228652"
 
     static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
     static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
